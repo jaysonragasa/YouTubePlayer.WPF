e9a555f251b84a1d766692d84d2898db99e33768
Jayson Ragasa <jayson.d.ragasa@live.com>
2020-05-14 20:29:56 +0800

    use win32api

--- YouTubePlayer.WPF/MainWindow.xaml.cs
+++ YouTubePlayer.WPF/MainWindow.xaml.cs
@@ -1,59 +1,91 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace YouTubePlayer.WPF
 {
     public partial class MainWindow : Window
     {
+        #region vars
+        DispatcherTimer _timer = new DispatcherTimer(DispatcherPriority.Normal)
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+
         CefSharp.Wpf.ChromiumWebBrowser _browser;
+
+        IntPtr _hWnd = IntPtr.Zero;
+        #endregion
+
+        #region ctor
 
         public MainWindow()
         {
             InitializeComponent();
 
-
             this.Loaded += MainWindow_Loaded;
         }
+        #endregion
+
+        #region events
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            //webBrowser.do
-            ////webBrowser.NavigateToString("<div style=\"position: fixed; z-index: -99; width: 100%; height: 100%\"><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/dLPeh8ew74Y\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div>");
-            //webBrowser.Source = new Uri("https://www.youtube-nocookie.com/embed/dJOHzKyxmn8", UriKind.Absolute);
-
-            _browser = new CefSharp.Wpf.ChromiumWebBrowser();
-            this.gridContent.Children.Add(_browser);
-            _browser.FrameLoadStart += (s, fl) =>
+            // get current window handle;
             {
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    this.tbYTID.IsEnabled = false;
-                });
-            };
-            _browser.FrameLoadEnd += (s, fl) =>
-            {
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    this.tbYTID.IsEnabled = true;
-                });
-            };
+                _hWnd = new WindowInteropHelper(this).Handle;
+            }
 
-            gridTitleBar.MouseDown += GridTitleBar_MouseDown;
-            gridParentContent.SizeChanged += GridParentContent_SizeChanged;
-            btnLoad.Click += BtnLoad_Click;
+            // create chrome browser.
+            // need to do this programatically because it's 
+            // bugging up when directly added in XAML
+            {
+                _browser = new CefSharp.Wpf.ChromiumWebBrowser();
+                this.gridContent.Children.Add(_browser);
+                _browser.FrameLoadStart += (s, fl) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        this.tbYTID.IsEnabled = false;
+                    });
+                };
+                _browser.FrameLoadEnd += (s, fl) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        this.tbYTID.IsEnabled = true;
+                    });
+                };
+            }
+
+            // init events
+            {
+                gridTitleBar.MouseDown += GridTitleBar_MouseDown;
+                gridTitleBar.MouseUp += GridTitleBar_MouseUp;
+                gridParentContent.SizeChanged += GridParentContent_SizeChanged;
+                btnLoad.Click += BtnLoad_Click;
+
+                // force window on top.
+                _timer.Tick += (s, te) =>
+                {
+                    ForceOnTop();
+                };
+            }
         }
 
         private void BtnLoad_Click(object sender, RoutedEventArgs e)
         {
-            if(this._browser != null)
+            if (this._browser != null)
             {
-                _browser.Address = $"https://www.youtube-nocookie.com/embed/{this.tbYTID.Text}";       
+                _browser.Address = $"https://www.youtube-nocookie.com/embed/{this.tbYTID.Text}";
             }
         }
 
         private void GridParentContent_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            if(e.NewSize.Width < 330 || e.NewSize.Height < 200)
+            if (e.NewSize.Width < 330 || e.NewSize.Height < 200)
             {
                 gridTitleBar.Visibility = Visibility.Collapsed;
             }
@@ -67,12 +99,26 @@ namespace YouTubePlayer.WPF
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
+                this._timer.Stop();
+
                 this.DragMove();
             }
-            else
-            {
-                this.ReleaseMouseCapture();
-            }
         }
+
+        private void GridTitleBar_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this._timer.Start();
+
+            this.ReleaseMouseCapture();
+        }
+        #endregion
+
+        #region methods
+
+        void ForceOnTop()
+        {
+            Win32API.SetWindowOnTop(this._hWnd);
+        }
+        #endregion
     }
 }
---
+++ YouTubePlayer.WPF/Win32API.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace YouTubePlayer.WPF
+{
+    public class Win32API
+    {
+        const int HWND_TOPMOST = -1;
+        const int HWND_NOTOPMOST = -2;
+        const int SWP_NOMOVE = 0x0002;
+        const int SWP_NOSIZE = 0x0001;
+        const int TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+
+        public static void SetWindowOnTop(IntPtr hWnd)
+        {
+            Win32API.SetWindowPos(
+                    //Process.GetCurrentProcess().Handle,
+                    hWnd,
+                    (IntPtr)Win32API.HWND_TOPMOST,
+                    //(int)this.Top, (int)this.Left, (int)(this.Left + this.Width), (int)(this.Top + this.Height),
+                    0, 0, 0, 0,
+                    Win32API.TOPMOST_FLAGS
+                    );
+        }
+    }
+}
